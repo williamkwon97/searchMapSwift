4e2e6d61834ed8964254e2f36832f7e6b9507623
William Kwon <williamkwon97@gmail.com>
2020-08-05 20:28:10 -0500

    add

--- LocationSearchTable.swift
+++ LocationSearchTable.swift
@@ -11,72 +11,53 @@ import UIKit
 import MapKit
 
 
-@IBOutlet weak var BarsSearchNoResultsLabel: UILabel!
- var mapView: MKMapView? = nil
-     var matchingItems = [MKAnnotation]()
-     var handleMapSearchDelegate:HandleMapSearch? = nil
- var allServiceLocations : [ServiceLocation] = []
-     
-     func updateSearchResults(for searchController: UISearchController) {
-         
+class LocationSearchTable: UITableViewController, UISearchResultsUpdating {
+    
+    var mapView: MKMapView? = nil
+        var matchingItems = [MKAnnotation]()
+        var handleMapSearchDelegate:HandleMapSearch? = nil
         
-         matchingItems = []
-         guard let mapView = mapView,
-             let searchBarText = searchController.searchBar.text else { return }
-         matchingItems = allServiceLocations.filter({ $0.locationName == searchBarText })
-                .map({
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(exactly: $0.latitude)!, longitude: CLLocationDegrees(exactly: $0.longitude)!)
-                    annotation.title = $0.locationName
-                 annotation.subtitle = $0.acronym
-                return annotation
-               })
-         
-        matchingItems = self.mapView!.annotations.filter { annotation -> Bool in
-         if annotation.title!?.range(of: searchBarText, options: .caseInsensitive) != nil {
-                    return true
+        func updateSearchResults(for searchController: UISearchController) {
+            
+            matchingItems = []
+            guard let mapView = mapView,
+                let searchBarText = searchController.searchBar.text else { return }
+            
+            for item in self.mapView!.annotations    {
+                let title = item.title! as! String
+                
+                if title.hasPrefix(searchBarText) && searchBarText != ""  {
+                    matchingItems.append(item)
                 }
-
-         if annotation.subtitle!?.range(of: searchBarText, options: .caseInsensitive) != nil {
-                    return true
-                }
-
-                return false
             }
-         self.tableView.reloadData()
-     }
-     
-     override func numberOfSections(in tableView: UITableView) -> Int {
-         return 1
-     }
-     
-     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         guard matchingItems.count != 0 else {
-             self.BarsSearchNoResultsLabel.isHidden = false
-             return 0
-         }
-         self.BarsSearchNoResultsLabel.isHidden = true
-         return matchingItems.count
-     }
-
-     
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
-         
-         let selectedItem = matchingItems[indexPath.row]
-         cell.textLabel?.text = selectedItem.title!
-         cell.detailTextLabel?.text = selectedItem.subtitle!
-         
-         
-         
-         return cell
-     }
-     
-     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         let selectedItem = matchingItems[indexPath.row]
-         handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
-         dismiss(animated: true, completion: nil)
-     }
-     
-     
- }
+            self.tableView.reloadData()
+        }
+        
+        override func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return matchingItems.count
+        }
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+            
+            let selectedItem = matchingItems[indexPath.row]
+            cell.textLabel?.text = selectedItem.title!
+            cell.detailTextLabel?.text = selectedItem.subtitle!
+            
+            
+            
+            return cell
+        }
+        
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let selectedItem = matchingItems[indexPath.row]
+            handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+            dismiss(animated: true, completion: nil)
+        }
+        
+        
+    }
