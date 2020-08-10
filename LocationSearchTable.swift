cab3be3d3d40a6d8ebfff2581210f7425719fe1d
William Kwon <williamkwon97@gmail.com>
2020-08-10 00:25:14 -0500

    Add code

--- LocationSearchTable.swift
+++ LocationSearchTable.swift
@@ -13,29 +13,30 @@ import MapKit
 
 class LocationSearchTable: UITableViewController, UISearchResultsUpdating {
     
+    @IBOutlet weak var BarsSearchNoResultsLabel: UILabel!
     var mapView: MKMapView? = nil
         var matchingItems = [MKAnnotation]()
         var handleMapSearchDelegate:HandleMapSearch? = nil
+    var allServiceLocations :  [ServiceLocation] = []
+    
         
         func updateSearchResults(for searchController: UISearchController) {
             
-            matchingItems = []
-            guard let mapView = mapView,
-                let searchBarText = searchController.searchBar.text else { return }
+           guard let mapView = mapView,
+           let searchBarText = searchController.searchBar.text else { return }
             
             
-            matchingItems = self.mapView!.annotations.filter { annotation -> Bool in
-                if annotation.title!.range(of: searchBarText, options: .caseInsensitive) != nil {
-                    return true
-                }
-
-                if annotation.subtitle!.range(of: searchBarText, options: .caseInsensitive) != nil {
-                    return true
-                }
-
-                return false
-            }
-
+            matchingItems = allServiceLocations.filter({ $0.locationName == searchBarText })
+                    .map({
+                        let annotation = MKPointAnnotation()
+                        annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(exactly: $0.latitude)!, longitude: CLLocationDegrees(exactly: $0.longitude)!)
+                        annotation.title = $0.locationName
+                        annotation.subtitle = $0.locationType.rawValue
+                    return annotation
+                   })
+            
+              
+          
             self.tableView.reloadData()
         }
         
@@ -44,8 +45,14 @@ class LocationSearchTable: UITableViewController, UISearchResultsUpdating {
         }
         
         override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            guard matchingItems.count != 0 else {
+                self.BarsSearchNoResultsLabel.isHidden = false
+                return 0
+            }
+            self.BarsSearchNoResultsLabel.isHidden = true
             return matchingItems.count
         }
+   
         
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
